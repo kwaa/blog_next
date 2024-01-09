1c2d4bcd84b1e918b1e79c1d549c583c9d08ef3d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-09 18:42:47 +0800

    Update serve.ts

--- serve.ts
+++ serve.ts
@@ -3,12 +3,6 @@ import { serveStatic } from 'hono/adapter/deno/serve-static.ts'
 
 const app = new Hono()
 
-app.get(
-  '*',
-  serveStatic({
-    root: './_site',
-    // onNotFound: (path, c) => {},
-  }),
-)
+app.get('*', serveStatic({ root: './_site' }))
 
 Deno.serve(app.fetch)
