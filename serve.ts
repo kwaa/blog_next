c95eb0fde4ef19b2a62688ad5e15d8867e5358e9
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-09 19:25:16 +0800

    try 404

--- serve.ts
+++ serve.ts
@@ -4,5 +4,6 @@ import { serveStatic } from 'hono/adapter/deno/serve-static.ts'
 const app = new Hono()
 
 app.get('*', serveStatic({ root: './_site' }))
+app.notFound(c => c.redirect('/404.html', 307))
 
 Deno.serve(app.fetch)
