ab6b8d2b98b354058b415d86cc32a526621fe070
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-20 00:16:57 +0800

    fix: move const instance

--- serve.ts
+++ serve.ts
@@ -2,9 +2,8 @@ import { Hono } from 'hono/mod.ts'
 import { serveStatic } from 'hono/adapter/deno/serve-static.ts'
 import { hatsuWellKnown, hatsuObject } from 'aoba/hono/middlewares/hatsu.ts'
 
-import { instance } from './_config.ts'
-
 const app = new Hono()
+const instance = new URL('https://hatsu.local')
 
 app.use('/.well-known/*', hatsuWellKnown({ instance }))
 app.use('/articles/*', hatsuObject({ instance }))
