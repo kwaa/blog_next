256a2a6eab4e880e70af141e2bdd4b9b9bfd0632
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-19 23:54:35 +0800

    chore: use aoba

--- _config.ts
+++ _config.ts
@@ -1,5 +1,12 @@
 import lume from 'lume/mod.ts'
 import lts from 'lume_theme_shiraha/mod.ts'
+import { hatsuPlugin as hatsu } from 'aoba/lume/plugins/hatsu.ts'
+
+export const instance = new URL('https://hatsu.local')
 
 export default lume({ src: './posts' })
   .use(lts())
+  .use(hatsu({
+    instance,
+    match: [/^\/articles\/(.+)$/],
+  }))
--- serve.ts
+++ serve.ts
@@ -1,8 +1,13 @@
 import { Hono } from 'hono/mod.ts'
 import { serveStatic } from 'hono/adapter/deno/serve-static.ts'
+import { hatsuWellKnown, hatsuObject } from 'aoba/hono/middlewares/hatsu.ts'
+
+import { instance } from './_config.ts'
 
 const app = new Hono()
 
+app.use('/.well-known/*', hatsuWellKnown({ instance }))
+app.use('/articles/*', hatsuObject({ instance }))
 app.get('*', serveStatic({ root: './_site' }))
 app.notFound(c => c.redirect('/404.html', 307))
 
