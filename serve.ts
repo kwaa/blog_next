3c6a1eb48676d687c5ac9bc69030f18fc15b9877
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-31 20:30:28 +0800

    chore: update hatsu instance

--- serve.ts
+++ serve.ts
@@ -3,7 +3,7 @@ import { serveStatic } from 'hono/adapter/deno/serve-static.ts'
 import { hatsuWellKnown, hatsuObject } from 'aoba/hono/middlewares/hatsu.ts'
 
 const app = new Hono()
-const instance = new URL('https://hatsu.local')
+const instance = new URL('https://hatsu-nightly-debug.hyp3r.link')
 
 app.use('/.well-known/*', hatsuWellKnown({ instance }))
 app.use('/articles/*', hatsuObject({ instance }))
