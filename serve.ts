23d8e215a5f66987ef75de60c75cdbc7518d5241
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-26 21:11:46 +0800

    chore(serve): fix types

--- serve.ts
+++ serve.ts
@@ -1,4 +1,4 @@
-import { Hono } from 'hono/mod.ts'
+import { type Context, Hono } from 'hono/mod.ts'
 import { serveStatic } from 'hono/adapter/deno/serve-static.ts'
 import { hatsuWellKnown, hatsuObject } from 'aoba/hono/middlewares/hatsu.ts'
 
@@ -8,6 +8,6 @@ const instance = new URL('https://hatsu-nightly-debug.hyp3r.link')
 app.use('/.well-known/*', hatsuWellKnown({ instance }))
 app.use('/articles/*', hatsuObject({ instance }))
 app.get('*', serveStatic({ root: './_site' }))
-app.notFound(({ redirect }) => redirect('/404.html', 307))
+app.notFound(({ redirect }: Context) => redirect('/404.html', 307))
 
 Deno.serve(app.fetch)
